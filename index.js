94007bd4bcec883aaec0804fc9056d00d80989ad
Nate Volker <natevolker@gmail.com>
2021-12-07 11:46:43 -0600

    test commit

--- index.js
+++ index.js
@@ -15,25 +15,34 @@ const namecheap = {
 async function run() {
 
   // create heroku app
+  console.log('creating app', 'post', '/apps', { body: { name: subdomain } })
+
   const app = await heroku.post(
     '/apps',
     { body: { name: subdomain } }
   )
 
+  console.log('app created', app)
+
   // add domain to heroku app
-  const { cname } = await heroku.post(
+  console.log('adding domain', 'post', `/apps/${app.name}/domains`, { body: { hostname: `${app.name}.${domain}`, sni_endpoint: null } })
+  const d = await heroku.post(
     `/apps/${app.name}/domains`,
     { body: { hostname: `${app.name}.${domain}`, sni_endpoint: null } }
   )
+  console.log('added domain', d)
 
   // get external IP
+  console.log('getting IP')
   const { stdout: ip } = await exec.getExecOutput(
     'dig',
     ['+short', 'myip.opendns.com', '@resolver1.opendns.com'],
     { silent: true }
   )
+  console.log('got IP', ip)
 
   // add CNAME record to namecheap
+  const { cname } = d
   const [sld, tld] = domain.split('.')
   const body = Object.entries({
     apiuser: namecheap.username,
@@ -60,7 +69,10 @@ async function run() {
     }
   ]
 
-  await fetch(...args)
+  console.log('adding cname record', args)
+  const response = await fetch(...args)
+  const result = await response.text()
+  console.log('added cname record', result)
 
   core.setOutput('url', `http://${app.name}.${domain}`)
 }
