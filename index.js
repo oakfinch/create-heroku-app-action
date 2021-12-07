5e071c03c65240a481711500701d5dcd434a8f40
Nate Volker <natevolker@gmail.com>
2021-12-07 12:38:58 -0600

    switch to cloudflare

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const core = require('@actions/core')
 const exec = require('@actions/exec')
 const Heroku = require('heroku-client')
 const { default: fetch } = require('node-fetch-cjs')
-const { FormData } = require('formdata-polyfill/esm.min.js')
 
 const domain = core.getInput('DOMAIN')
 const subdomain = core.getInput('SUBDOMAIN')
 const heroku = new Heroku({ token: core.getInput('HEROKU_API_TOKEN') })
-const namecheap = {
-  username: core.getInput('NAMECHEAP_USERNAME'),
-  key: core.getInput('NAMECHEAP_API_KEY'),
+const cloudflare = {
+  token: core.getInput('CLOUDFLARE_TOKEN'),
+  zone: core.getInput('CLOUDFLARE_ZONE'),
 }
 
 async function run() {
@@ -48,29 +47,20 @@ async function run() {
 
   // add CNAME record to namecheap
   const { cname } = d
-  const [sld, tld] = domain.split('.')
-  const body = Object.entries({
-    apiuser: namecheap.username,
-    apikey: namecheap.key,
-    username: namecheap.username,
-    Command: 'namecheap.domains.dns.setHosts',
-    ClientIp: ip.trim(),
-    SLD: sld,
-    TLD: tld,
-    HostName1: app.name,
-    RecordType1: 'CNAME',
-    Address1: cname,
-    TTL1: 100
-  }).reduce((acc, [key, val]) => {
-    acc.append(key, val)
-    return acc
-  }, new FormData())
-
   const args = [
-    "https://api.namecheap.com/xml.response",
+    `https://api.cloudflare.com/client/v4/zones/${cloudflare.zone}/dns_records`,
     {
       method: 'POST',
-      body
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${cloudflare.token}`
+      },
+      body: {
+        type: 'CNAME',
+        name: app.name,
+        content: cname,
+        ttl: 60
+      }
     }
   ]
 
